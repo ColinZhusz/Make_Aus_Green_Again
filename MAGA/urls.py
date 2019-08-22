e32828f6ad8c4c955a1a11989d6ee65e31f327e2
Jun Zhu <markzhusz@gmail.com>
2019-08-22 17:20:41 +1000

    Version 1

--- MAGA/urls.py
+++ MAGA/urls.py
@@ -2,18 +2,19 @@ from django.conf.urls import url
 from MAGA import views
 urlpatterns = [
     #index page
-    url(r'^$', views.index, name='Make Our Australia Green Again'),
+    url(r'^$', views.index, name='home'),
+
 	#Education page
-    url(r'Education',views.edu,name = 'edu page'),
+    url(r'Education',views.edu,name = 'education'),
 
     #Information page
-    url(r'Information',views.info, name ='Information'),
+    url(r'Information',views.info, name ='information'),
 
     #Classification
-    url(r'Classification', views.cla, name='Transformation'),
+    url(r'Classification', views.cla, name='classification'),
 
 
     #Transformation
-    url(r'Transformation', views.trans, name='Transformation'),
+    url(r'Transformation', views.trans, name='transformation'),
 
 ]
--- MAGA/views.py
+++ MAGA/views.py
@@ -9,17 +9,7 @@ def index(request):
 
 
 def edu(request):
-    # 获取前端post过来的用户名和密码
-    name = request.POST.get('name', None)
-    password = request.POST.get('password', None)
-
-    # 把用户和密码组装成字典
-    data = {'name': name, 'password': password}
-    list.append(data)
-
-    return render(request, 'Education.html',
-                  {'form': list})  # 通过render模块把index.html这个文件返回到前端，并且返回给了前端一个变量form，在写html时可以调用这个form来展示list里的内容
-
+  return render_to_response('Education.html')
 
 def info(request):
     return render_to_response('Information.html')
