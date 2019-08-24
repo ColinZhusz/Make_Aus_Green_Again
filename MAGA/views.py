53c2d47b8f44b40e67dd4d0a7c64295950c8660e
JaniceZhu <janiceqqagz@163.com>
2019-08-24 17:57:19 +1000

    undo the redirect

--- MAGA/views.py
+++ MAGA/views.py
@@ -6,15 +6,15 @@ from django.template import RequestContext, loader
 appname = 'Make Our Australia Green Again'
 
 def index(request):
-    ...
-    return redirect("http://18.221.149.227/MAGA/")
-
+#     ...
+#     return redirect("http://18.221.149.227/MAGA/")
 #
-# def home(request):
-#     context = {
-#         'appname': appname,
-#     }
-#     return render(request, 'index.html', context)
+# #
+# # def home(request):
+    context = {
+        'appname': appname,
+    }
+    return render(request, 'index.html', context)
 
 
 def education(request):
