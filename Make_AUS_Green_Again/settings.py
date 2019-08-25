002f14b03be0afa5d0ef4caa6cdd479f0372714d
JaniceZhu <janiceqqagz@163.com>
2019-08-26 00:42:51 +1000

    Update setting to unshow debug message

--- Make_AUS_Green_Again/settings.py
+++ Make_AUS_Green_Again/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '94=obs1pgr)si@2eca$bcu9ca8=o+&^u!k@s4#d*blj=o2oy6a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # ALLOWED_HOSTS update
