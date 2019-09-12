a0fbfc7d7a0ccbc5186a702569bc1472f4d9d3ab
JaniceZhu <janiceqqagz@163.com>
2019-09-12 17:38:29 +1000

    revert to v1

--- img_db/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImgDbConfig(AppConfig):
-    name = 'img_db'
--- img_db/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class IMG(models.Model):
-    img = models.ImageField(upload_to='img')
-    name = models.CharField(max_length=20)
