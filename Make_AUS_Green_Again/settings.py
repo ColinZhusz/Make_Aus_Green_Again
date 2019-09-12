85405f21a66da89daeba4027ab49669d914de45f
JaniceZhu <janiceqqagz@163.com>
2019-09-12 20:07:56 +1000

    undo setting

--- Make_AUS_Green_Again/settings.py
+++ Make_AUS_Green_Again/settings.py
@@ -135,5 +135,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
+# MEDIA_URL = './media/'
