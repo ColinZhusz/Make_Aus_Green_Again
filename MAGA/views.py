27aee1662f7c9326772d9f006de6ddee06b42751
JaniceZhu <janiceqqagz@163.com>
2019-09-16 18:47:19 +1000

    function rebuild

--- MAGA/views.py
+++ MAGA/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 from django.utils import timezone
 from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 
 from MAGA.models import *
+
 appname = 'Make Australia Green Again'
+
 
 def index(request):
     context = {
@@ -14,17 +16,20 @@ def index(request):
     userCreate(request)
     return render(request, 'index.html', context)
 
+
 def about(request):
     context = {
         'appname': appname,
     }
     return render(request, 'about.html', context)
 
+
 def education(request):
     context = {
         'appname': appname,
     }
     return render(request, 'education.html', context)
+
 
 def information(request):
     context = {
@@ -33,12 +38,12 @@ def information(request):
     return render(request, 'information.html', context)
 
 
-
 def transformation(request):
     context = {
         'appname': appname,
     }
     return render(request, 'transformation.html', context)
+
 
 def calculator(request):
     context = {
@@ -48,31 +53,48 @@ def calculator(request):
 
 
 def classification(request):
-
     if request.method == 'POST':
-        img = Uploaded_photo(photo_ad=request.FILES.get('img'))
+        ip = userIp(request)
+        if detectUser(ip):
+            u = User.objects.filter(ip_ad=ip).get()
+            img = Uploaded_photo(photo_ad=request.FILES.get('img'), user=u)
+        else:
+            user = User(ip)
+            user.save()
+            img = Uploaded_photo(photo_ad=request.FILES.get('img'), user=user)
         img.save()
-        return HttpResponse('upload ok')
     context = {
         'appname': appname,
     }
-    return render(request, 'classification.html',context)
+    return render(request, 'classification.html', context)
+
 
 def showImg(request):
     imgs = Uploaded_photo.objects.all()
     context = {
-        'img' : imgs
+        'img': imgs
     }
     return render(request, 'showImg.html', context)
 
+
 def userCreate(request):
-    ip = request.META['REMOTE_ADDR']
-    if 'HTTP_X_FORWARDED_FOR' in request.META:
-        ip = request.META['HTTP_X_FORWARDED_FOR']
-    if User.objects.filter(ip_ad=ip):
+    ip = userIp(request)
+    if detectUser(ip):
         pass
     else:
         user = User(ip_ad=ip)
         user.save()
 
-    pass
+
+def detectUser(ip):
+    if User.objects.filter(ip_ad=ip).exists():
+        return True
+    else:
+        return False
+
+
+def userIp(request):
+    ip = request.META['REMOTE_ADDR']
+    if 'HTTP_X_FORWARDED_FOR' in request.META:
+        ip = request.META['HTTP_X_FORWARDED_FOR']
+    return ip
