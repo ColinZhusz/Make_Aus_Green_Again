d4101bbc597b4d6d4944c44ce05d0d28360bb748
sakshamnagpal <45676474+sakshamnagpal@users.noreply.github.com>
2019-09-19 16:56:38 +1000

    FIxed money calculation

--- MAGA/static/js/slider.js
+++ MAGA/static/js/slider.js
@@ -70,6 +70,7 @@ window.onload = function()
     document.getElementById("TreeOxyVal").innerHTML = (trees/2).toFixed(2);
     document.getElementById("TreeVal").innerHTML = (300/trees).toFixed(2);
     document.getElementById("HolidayVal").innerHTML = (money/120).toFixed(0);
+    document.getElementById("GroceryVal").innerHTML = (money/400).toFixed(0);
 
     // compare with the national average
     if ((co2/1000) > 22 ){
@@ -123,6 +124,7 @@ window.onload = function()
     document.getElementById("TreeOxyVal").innerHTML = (trees/2).toFixed(2);
     document.getElementById("TreeVal").innerHTML = (300/trees).toFixed(2);
     document.getElementById("HolidayVal").innerHTML = (money/120).toFixed(0);
+    document.getElementById("GroceryVal").innerHTML = (money/400).toFixed(0);
 
     // Set grayscale for the tree image according to the value
     var gray_trees = (trees/20)*100;
@@ -180,6 +182,7 @@ window.onload = function()
     document.getElementById("TreeOxyVal").innerHTML = (trees/2).toFixed(2);
     document.getElementById("TreeVal").innerHTML = (300/trees).toFixed(2);
     document.getElementById("HolidayVal").innerHTML = (money/120).toFixed(0);
+    document.getElementById("GroceryVal").innerHTML = (money/400).toFixed(0);
 
     // Set grayscale for the tree image according to the value
     var gray_trees = (trees/20)*100;
@@ -238,6 +241,7 @@ window.onload = function()
     document.getElementById("TreeOxyVal").innerHTML = (trees/2).toFixed(2);
     document.getElementById("TreeVal").innerHTML = (300/trees).toFixed(2);
     document.getElementById("HolidayVal").innerHTML = (money/120).toFixed(0);
+    document.getElementById("GroceryVal").innerHTML = (money/400).toFixed(0);
 
     // Set grayscale for the tree image according to the value
     var gray_trees = (trees/20)*100;
